28d7ee306664918f7d5595a56dca2f359c430e88
codemangg <florian.nadegger@gmx.at>
2023-10-28 17:28:09 +0200

    changes

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,9 @@
+// Run the code after the DOM has fully loaded.
 document.addEventListener("DOMContentLoaded", function () {
 
+    // Check if we're on the "impressum.html" page.
     if (window.location.pathname.indexOf("impressum.html") === -1) {
+        // Initialize map, smooth scrolling, and slideshow if NOT on "impressum.html".
         initMap();
         initSmoothScroll();
         initSlideshow();
@@ -8,19 +11,25 @@ document.addEventListener("DOMContentLoaded", function () {
         console.log("We're on the impressum.html page. Skipping initMap, initSmoothScroll, and initSlideshow.");
     }
 
+    // Initialize the hamburger menu and menu swipe functionalities.
     initHamburgerMenu();
     initMenuSwipe();
 });
 
+/**
+ * Initialize the map on the page using Leaflet.
+ */
 function initMap() {
-
+    // Check if the map element exists on the page.
     if (!document.getElementById('map')) return;
 
+    // Set up the map with a default view.
     var map = L.map('map').setView([47.3185068, 13.1383278], 17);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
+    // Create a custom marker icon.
     var customIcon = L.icon({
         iconUrl: './images/marker-icon.png',
         shadowUrl: './images/marker-shadow.png',
@@ -31,6 +40,7 @@ function initMap() {
         popupAnchor: [0, -20]
     });
 
+    // Add the custom marker to the map and bind a popup to it.
     var marker = L.marker([47.3185068, 13.1383278], { icon: customIcon }).addTo(map)
         .bindPopup('<img src="./logos/Gerardo.jpg" alt="GERARDO Logo" width="200"><br><strong>Pub-Bar Gerardo</strong><br>Goldegger Straße 6<br>5620 Schwarzach', {
             autoPanPadding: [20, 20]
@@ -40,8 +50,10 @@ function initMap() {
     map.setView(marker.getLatLng());
 }
 
+/**
+ * Initialize smooth scrolling for anchor links on the page.
+ */
 function initSmoothScroll() {
-
     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         anchor.addEventListener('click', function (e) {
             e.preventDefault();
@@ -49,25 +61,33 @@ function initSmoothScroll() {
             const targetElement = document.querySelector('#' + targetId);
 
             if (targetId === 'images') {
+                // Open the slideshow.
                 openSlideshow();
             } else if (targetId !== 'impressum.html' && targetElement) {
+                // Smooth scroll to the target element.
                 targetElement.scrollIntoView({ behavior: 'smooth' });
             } else if (targetId === 'impressum.html') {
+                // Navigate to "impressum.html".
                 window.location.href = './pages/impressum.html';
             }
         });
     });
 }
 
+// Image gallery data and current index.
 let currentImageIndex = 0;
 const images = ['bildgalerie/image1.jpg', 'bildgalerie/image2.jpg', 'bildgalerie/image3.jpg', 'bildgalerie/image4.jpg', 'bildgalerie/image5.jpg'];
 
+/**
+ * Initialize the slideshow functionality.
+ */
 function initSlideshow() {
-
+    // Check if the slideshow element exists on the page.
     if (!document.getElementById('slideshow')) return;
 
     document.querySelector('a[href="#images"]').addEventListener('click', openSlideshow);
 
+    // Handle keyboard inputs for slideshow navigation.
     document.addEventListener('keydown', function (event) {
         if (document.getElementById('slideshow').style.display === "block") {
             switch (event.key) {
@@ -78,6 +98,7 @@ function initSlideshow() {
         }
     });
 
+    // Handle touch gestures for the slideshow.
     let touchStartX = null;
     const slideshowEl = document.getElementById('slideshow');
 
@@ -90,6 +111,7 @@ function initSlideshow() {
         let diffX = touchStartX - touchEndX;
 
         if (Math.abs(diffX) > 50) {
+            // Change slide based on the swipe direction.
             changeSlide(diffX > 0 ? 1 : -1);
         }
 
@@ -97,6 +119,9 @@ function initSlideshow() {
     });
 }
 
+/**
+ * Open the image slideshow.
+ */
 function openSlideshow() {
     const slideshow = document.getElementById('slideshow');
     const slideshowImg = document.getElementById('slideshowImg');
@@ -106,33 +131,47 @@ function openSlideshow() {
     slideshow.style.display = "block";
 }
 
+/**
+ * Close the image slideshow.
+ */
 function closeSlideshow() {
     document.getElementById('slideshow').style.display = "none";
 }
 
+/**
+ * Change the slide of the image slideshow.
+ * @param {number} n - The direction to change the slide.
+ */
 function changeSlide(n) {
     currentImageIndex = (currentImageIndex + n + images.length) % images.length;
     document.getElementById('slideshowImg').src = images[currentImageIndex];
 }
 
+/**
+ * Initialize the hamburger menu toggle functionality.
+ */
 function initHamburgerMenu() {
-
     const hamburgerMenu = document.getElementById('hamburger-menu');
     const mobileNavMenu = document.getElementById('mobile-nav-menu');
     const hamburgerMenuMobile = document.getElementById('hamburger-menu-docked');
 
+    // Check if mobile navigation exists.
     if (!mobileNavMenu) return;
 
+    // Toggle the mobile menu when clicked.
     const toggle = () => toggleMenu();
 
     if (hamburgerMenu) hamburgerMenu.addEventListener('click', toggle);
     if (hamburgerMenuMobile) hamburgerMenuMobile.addEventListener('click', toggle);
 
+    // Close the mobile menu when a link inside it is clicked.
     mobileNavMenu.querySelectorAll('a').forEach(link => link.addEventListener('click', toggle));
 }
 
+/**
+ * Initialize touch swipe functionality for the navigation menu.
+ */
 function initMenuSwipe() {
-
     let touchStartXMenu = null;
     let touchEndXMenu = null;
 
@@ -146,12 +185,18 @@ function initMenuSwipe() {
     });
 }
 
+/**
+ * Handle the swipe gesture for the mobile navigation menu.
+ * @param {number} touchStartXMenu - The starting X-coordinate of the swipe.
+ * @param {number} touchEndXMenu - The ending X-coordinate of the swipe.
+ */
 function handleMenuSwipeGesture(touchStartXMenu, touchEndXMenu) {
     let slideshow = document.getElementById('slideshow');
     if (!touchStartXMenu || !touchEndXMenu || (slideshow && slideshow.style.display === "block")) return;
 
     let diffX = touchStartXMenu - touchEndXMenu;
     if (Math.abs(diffX) > 100) {
+        // Toggle the mobile menu based on swipe direction.
         if (diffX > 0 && document.getElementById('mobile-nav-menu').classList.contains('open')) {
             toggleMenu();
         } else if (diffX < 0 && !document.getElementById('mobile-nav-menu').classList.contains('open')) {
@@ -160,11 +205,15 @@ function handleMenuSwipeGesture(touchStartXMenu, touchEndXMenu) {
     }
 }
 
+/**
+ * Toggle the mobile navigation menu.
+ */
 function toggleMenu() {
     let nav = document.getElementById('mobile-nav-menu');
 
     if (!nav) return;
 
+    // Toggle the 'open' class to open/close the mobile menu.
     if (nav.classList.contains('open')) {
         nav.classList.remove('open');
     } else {
